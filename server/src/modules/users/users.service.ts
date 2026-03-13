be052bc2f57b74982ac1953af7e1c7f2325af565
calli97 <nahuelcallalli@gmail.com>
2026-03-13 16:02:23 -0300

    fix get users

--- server/src/modules/users/users.service.ts
+++ server/src/modules/users/users.service.ts
@@ -56,7 +56,7 @@ export class UsersService {
   }
 
   findAll(): Promise<User[]> {
-    return this.usersRepository.find();
+    return this.usersRepository.find({ relations: { roles: true } });
   }
 
   async findOne(id: number): Promise<User> {
