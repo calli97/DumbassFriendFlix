eb91e59ec2755c1f803840df7459d42dd4ec1947
calli97 <nahuelcallalli@gmail.com>
2026-03-28 16:19:27 -0300

    format

--- server/src/modules/media/media.module.ts
+++ server/src/modules/media/media.module.ts
@@ -1,8 +1,8 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { MediaController } from './media.controller';
-import { MediaService } from './media.service';
-import { Media } from './entities/media.entity';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { MediaController } from "./media.controller";
+import { MediaService } from "./media.service";
+import { Media } from "./entities/media.entity";
 
 @Module({
   imports: [TypeOrmModule.forFeature([Media])],
