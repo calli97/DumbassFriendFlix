170fa2972c56dc8d01f3ba2ad56789f25e2c9b15
calli97 <nahuelcallalli@gmail.com>
2026-03-28 16:16:23 -0300

    more format

--- server/src/modules/users/entities/user.entity.ts
+++ server/src/modules/users/entities/user.entity.ts
@@ -6,37 +6,37 @@ import {
   UpdateDateColumn,
   ManyToMany,
   JoinTable,
-} from 'typeorm';
-import { Exclude } from 'class-transformer';
-import { Role } from './role.entity';
+} from "typeorm";
+import { Exclude } from "class-transformer";
+import { Role } from "./role.entity";
 
-@Entity('users')
+@Entity("users")
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ type: 'varchar', length: 100, unique: true })
+  @Column({ type: "varchar", length: 100, unique: true })
   username: string;
 
-  @Column({ type: 'varchar', length: 255, unique: true })
+  @Column({ type: "varchar", length: 255, unique: true })
   email: string;
 
   // Excluded from all serialized responses via ClassSerializerInterceptor
   @Exclude()
-  @Column({ type: 'varchar', length: 255 })
+  @Column({ type: "varchar", length: 255 })
   password: string;
 
-  @CreateDateColumn({ name: 'created_at' })
+  @CreateDateColumn({ name: "created_at" })
   createdAt: Date;
 
-  @UpdateDateColumn({ name: 'updated_at' })
+  @UpdateDateColumn({ name: "updated_at" })
   updatedAt: Date;
 
   @ManyToMany(() => Role, (role) => role.users)
   @JoinTable({
-    name: 'user_roles',
-    joinColumn: { name: 'user_id', referencedColumnName: 'id' },
-    inverseJoinColumn: { name: 'role_id', referencedColumnName: 'id' },
+    name: "user_roles",
+    joinColumn: { name: "user_id", referencedColumnName: "id" },
+    inverseJoinColumn: { name: "role_id", referencedColumnName: "id" },
   })
   roles: Role[];
 }
