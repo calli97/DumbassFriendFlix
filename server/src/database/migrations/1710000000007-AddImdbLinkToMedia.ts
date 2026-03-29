44c56ec6f8352401193409c1803badb3e9efa869
calli97 <nahuelcallalli@gmail.com>
2026-03-29 17:18:37 -0300

    add sync

---
+++ server/src/database/migrations/1710000000007-AddImdbLinkToMedia.ts
@@ -0,0 +1,17 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AddImdbLinkToMedia1710000000007 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+      ALTER TABLE \`media\`
+      ADD COLUMN \`imdb_link\` VARCHAR(255) NULL
+    `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+      ALTER TABLE \`media\`
+      DROP COLUMN \`imdb_link\`
+    `);
+  }
+}
